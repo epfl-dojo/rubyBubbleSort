08f314f860f945de6dc5ea485c09cc13271e4238
SaphireVert <jerome.cosandey@gmail.com>
2020-10-02 09:44:19 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,33 @@
+#procedure bubbleSort(A : list of sortable items)
+#    n := length(A)
+#    repeat
+#        swapped := false
+#        for i := 1 to n-1 inclusive do
+#            /* if this pair is out of order */
+#            if A[i-1] > A[i] then
+#                /* swap them and remember something changed */
+#                swap(A[i-1], A[i])
+#               swapped := true
+#            end if
+#        end for
+#    until not swapped
+#end procedure
+
+# https://fr.wikipedia.org/wiki/Tri_à_bulles
+
+
+def bubbleSort(tableau) 
+  n = tableau.length()
+  loop do
+    swapper = false
+    for i in 1..n-1 do
+      if tableau[i-1] > tableau[i]
+        tableau[i],tableau[i-1] = tableau[i-1], tableau[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  puts tableau
+end
+bubbleSort([1,4,6,3.32345,3,0.3,2])
